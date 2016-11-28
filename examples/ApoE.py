fa6decf452ba96faa1116d5e198ae634f6f84a15
Reece Hart <reecehart@gmail.com>
2016-11-27 21:23:02 -0800

    use dicts in ApoE demo for better generalizability

--- examples/ApoE.py
+++ examples/ApoE.py
@@ -11,25 +11,29 @@ from vmc.models import ObjectReference, Interval, Allele, Haplotype, Genotype
 
 sr = ObjectReference(namespace="NCBI", accession="NC_000019.10")
 
-i429358 = Interval(44908683, 44908684)
-i7412 = Interval(44908821, 44908822)
+intervals = {
+    "rs429358": Interval(44908683, 44908684),
+    "rs7412": Interval(44908821, 44908822),
+    }
 
-rs429358T = Allele(sr, i429358, "T")
-rs429358C = Allele(sr, i429358, "C")
-rs7412T = Allele(sr, i7412, "T")
-rs7412C = Allele(sr, i7412, "C")
+alleles = {
+    "rs429358T": Allele(sr, intervals["rs429358"], "T"),
+    "rs429358C": Allele(sr, intervals["rs429358"], "C"),
+    "rs7412T":   Allele(sr, intervals["rs7412"],   "T"),
+    "rs7412C":   Allele(sr, intervals["rs7412"],   "C"),
+    }
 
 name_hap_map = {
-    "ε1": Haplotype([rs429358C, rs7412T]),
-    "ε2": Haplotype([rs429358T, rs7412T]),
-    "ε3": Haplotype([rs429358T, rs7412C]),
-    "ε4": Haplotype([rs429358C, rs7412C]),
+    "ε1": Haplotype([alleles["rs429358C"], alleles["rs7412T"]]),
+    "ε2": Haplotype([alleles["rs429358T"], alleles["rs7412T"]]),
+    "ε3": Haplotype([alleles["rs429358T"], alleles["rs7412C"]]),
+    "ε4": Haplotype([alleles["rs429358C"], alleles["rs7412C"]]),
     }
 hap_name_map = {str(h.digest()): n for n, h in name_hap_map.items()}
 
-haps = list(name_hap_map.values())
+haplotypes = list(name_hap_map.values())
 
-gts = [Genotype([haps[i], haps[j]])
-       for i in range(len(haps))
-       for j in range(i, len(haps))]
+genotypes = [Genotype([haplotypes[i], haplotypes[j]])
+             for i in range(len(haplotypes))
+             for j in range(i, len(haplotypes))]
 
