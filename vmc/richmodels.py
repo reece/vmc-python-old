dd13168922fc7696499b941d16d99df2f6c9f6fa
Reece Hart <reecehart@gmail.com>
2017-02-25 15:32:20 -0800

    revert to richmodels with object references; as_dict returns id-based dicts

--- vmc/richmodels.py
+++ vmc/richmodels.py
@@ -11,15 +11,21 @@ from vmc.digest import vmc_digest
 
 @attr.s
 @six.python_2_unicode_compatible
-class ObjectReference(object):
+class Identifier(object):
     namespace = attr.ib(validator=instance_of(six.text_type))
     accession = attr.ib(validator=instance_of(six.text_type))
 
+    def __bytes__(self):
+        return self.__str__().encode("UTF-8")
+
     def __str__(self):
-        return "{self.namespace}:{self.accession}".format(self=self)
+        return self.as_uri()
 
     def as_dict(self):
         return attr.asdict(self)
+
+    def as_uri(self):
+        return "{self.namespace}:{self.accession}".format(self=self)
 
     def validate(self):
         attr.validate(self)
@@ -36,8 +42,11 @@ class Interval(Location):
     start = attr.ib(validator=instance_of(int))
     end = attr.ib(validator=instance_of(int))
 
+    def __bytes__(self):
+        return self.__str__().encode("UTF-8")
+
     def __str__(self):
-        return "<{self.start},{self.end}>".format(self=self)
+        return "{self.start}:{self.end}".format(self=self)
 
     def as_dict(self):
         return attr.asdict(self)
@@ -52,104 +61,193 @@ class Interval(Location):
 
 
 @attr.s
-class Allele(object):
-    seqref = attr.ib(validator=instance_of(ObjectReference))
+class Locus(object):
+    seqref = attr.ib(validator=instance_of(str))
     location = attr.ib(validator=instance_of(Location))
-    replacement = attr.ib(validator=instance_of(six.text_type))
-    id = attr.ib(default=None, validator=optional(instance_of(ObjectReference)))
-
-    #def __str__(self):
-    #    return "{self.seqref}:{self.location}:{self.replacement}".format(self=self)
-
-    def as_dict(self):
-        return {
-            "seqref": self.seqref.as_dict(),
-            "location": self.location.as_dict(),
-            "replacement": self.replacement,
-            "id": str(self.digest)
-            }
+    id = attr.ib(default=None, validator=optional(instance_of(str)))
 
     @property
+    def identifier(self):
+        if self.id is None:
+            self.id = self.computed_identifer()
+        return self.id
+
+    def __bytes__(self):
+        return self.__str__().encode("UTF-8")
+
+    def __str__(self):
+        return "{self.seqref}:{self.location}".format(self=self)
+
+    def as_dict(self):
+        return attr.asdict(self)
+
+    def computed_identifer(self):
+        return "GL:" + self.digest()
+
     def digest(self):
-        binary_rep = "{self.seqref}:{self.location}:{self.replacement}".format(self=self).encode("UTF-8")
-        return ObjectReference("GA", vmc_digest(binary_rep))
+        return vmc_digest(bytes(self))
 
     def validate(self):
         attr.validate(self)
+        self.seqref.validate()
+        self.location.validate()
+
+
+@attr.s
+class Allele(object):
+    seqref = attr.ib(validator=instance_of(str))
+    location = attr.ib(validator=instance_of(Location))
+    replacement = attr.ib(validator=instance_of(six.text_type))
+    id = attr.ib(default=None, validator=optional(instance_of(str)))
+
+    @property
+    def identifier(self):
+        if self.id is None:
+            self.id = self.computed_identifer()
+        return self.id
+
+    def __bytes__(self):
+        return self.__str__().encode("UTF-8")
+
+    def __str__(self):
+        return "{self.seqref}:{self.location}:{self.replacement}".format(self=self)
+
+    def as_dict(self):
+        return attr.asdict(self)
+
+    def computed_identifer(self):
+        return "GA:" + self.digest()
+
+    def digest(self):
+        return vmc_digest(bytes(self))
+
+    def validate(self):
+        attr.validate(self)
+        self.locus.validate()
+        self.validate()
 
 
 @attr.s
 class Haplotype(object):
     alleles = attr.ib(validator=instance_of(list))
-    id = attr.ib(default=None, validator=optional(instance_of(ObjectReference)))
+    id = attr.ib(default=None, validator=optional(instance_of(str)))
 
     @property
+    def identifier(self):
+        if self.id is None:
+            self.id = self.computed_identifer()
+        return self.id
+
     def allele_ids(self):
-        return list(sorted(str(a.digest) for a in self.alleles))
+        # return unicode allele identifiers in UTF-8 encoded order
+        # encoding eliminates effects of locale-specific collation order 
+        ids = [o.identifier for o in self.alleles]
+        ids.sort(key=lambda e: e.encode("UTF-8"))
+        return ids
 
     def as_dict(self):
         return {
-            "allele_ids": list(map(str, self.allele_ids)),
-            "id": str(self.digest)
-            }
+            "allele_ids": self.allele_ids(),
+            "id": self.identifier
+        }
 
-    @property
+    def computed_identifer(self):
+        return "GH:" + self.digest()
+
     def digest(self):
-        binary_rep = ";".join(self.allele_ids).encode("UTF-8")
-        return ObjectReference("GH", vmc_digest(binary_rep))
-
-    def seqref(self):
-        self.validate()
-        return self.alleles[0].seqref
+        return vmc_digest(";".join(self.allele_ids()).encode("UTF-8"))
 
     def validate(self):
         attr.validate(self)
-        for a in self.alleles:
-            a.validate()
-        if len(set(a.seqref for a in self.alleles)) != 1:
-            raise ValueError("Haplotype alleles must be defined on the same sequence reference")
-        for i in range(len(self.alleles) - 1):
-            if self.alleles[i].location.start > self.alleles[i + 1].location.start:
-                raise ValueError("Haplotype alleles must be ordered")
-            if self.alleles[i].location.end > self.alleles[i + 1].location.start:
-                raise ValueError("Haplotype alleles may not overlap")
+        if len(set(a.locus.seqref for a in self.alleles)) != 1:
+            raise ValueError("Haplotype alleles must be defined on exactly one sequence")
+        # TODO: validate that alleles do not overlap
+
 
 
 @attr.s
 class Genotype(object):
     haplotypes = attr.ib(validator=instance_of(list))
-    id = attr.ib(default=None, validator=optional(instance_of(ObjectReference)))
+    id = attr.ib(default=None, validator=optional(instance_of(str)))
+
+    @property
+    def identifier(self):
+        if self.id is None:
+            self.id = self.computed_identifier()
+        return self.id
 
     def as_dict(self):
         return {
-            "haplotype_ids": list(map(str, self.haplotype_ids)),
-            "id": str(self.digest)
-            }
+            "haplotype_ids": self.haplotype_ids(),
+            "id": self.identifier
+        }
 
-    @property
-    def haplotype_ids(self):
-        return list(sorted(str(h.digest) for h in self.haplotypes))
+    def computed_identifier(self):
+        return "GG:" + self.digest()
 
-    @property
     def digest(self):
-        binary_rep = ";".join(self.haplotype_ids).encode("UTF-8")
-        return ObjectReference("GG", vmc_digest(binary_rep))
+        return vmc_digest(";".join(self.haplotype_ids()).encode("UTF-8"))
 
-    def location(self):
-        self.validate()
-        return self.alleles[0].location
-
-    def seqref(self):
-        self.validate()
-        return self.alleles[0].seqref
+    def haplotype_ids(self):
+        # return unicode haplotype identifiers in UTF-8 encoded order
+        # encoding eliminates effects of locale-specific collation order 
+        ids = [o.identifier for o in self.haplotypes]
+        ids.sort(key=lambda e: e.encode("UTF-8"))
+        return ids
 
     def validate(self):
         attr.validate(self)
-        for a in self.alleles:
-            a.validate()
-        if len(set(a.seqref for a in self.alleles)) != 1:
-            raise ValueError("Genotype alleles must be defined on the same sequence reference")
-        if len(set(a.location for a in self.alleles)) != 1:
-            raise ValueError("Genotype alleles must be defined at the same Location")
-        # TODO: Ensure Haplotypes are ordered (see haplotype ordering above)
 
+
+if __name__ == "__main__":
+    import json
+
+    import vmc.codecs.json
+
+    def to_json(o):
+        return json.dumps(o, indent=2, sort_keys=True, cls=vmc.codecs.json.JSONEncoder, ensure_ascii=False)
+
+
+    sr = "NCBI:NC_000019.10"
+    intervals = {
+        "rs429358": Interval(44908683, 44908684),
+        "rs7412": Interval(44908821, 44908822),
+    }
+    o = intervals["rs429358"]
+    print("r={o!r}\ns={o}\nj={j}".format(o=o, j=to_json(o)))
+
+
+    loci = {
+        "rs429358": Locus(sr, intervals["rs429358"]),
+        "rs7412": Locus(sr, intervals["rs7412"]),
+    }
+    o = loci["rs429358"]
+    print("r={o!r}\ns={o}\nid={o.identifier}\nj={j}".format(o=o, j=to_json(o)))
+
+
+    alleles = {
+        "rs429358T": Allele(sr, intervals["rs429358"], "T"),
+        "rs429358C": Allele(sr, intervals["rs429358"], "C"),
+        "rs7412T":   Allele(sr, intervals["rs7412"],   "T"),
+        "rs7412C":   Allele(sr, intervals["rs7412"],   "C"),
+    }
+    o = alleles["rs429358C"]
+    print("r={o!r}\ns={o}\nid={o.identifier}\nj={j}".format(o=o, j=to_json(o)))
+
+    haplotypes = {
+        "ε1": Haplotype([alleles["rs429358C"], alleles["rs7412T"]]),
+        "ε2": Haplotype([alleles["rs429358T"], alleles["rs7412T"]]),
+        "ε3": Haplotype([alleles["rs429358T"], alleles["rs7412C"]]),
+        "ε4": Haplotype([alleles["rs429358C"], alleles["rs7412C"]]),
+        "ε4r": Haplotype([alleles["rs7412C"], alleles["rs429358C"]]),
+    }
+    o = haplotypes["ε1"]
+    print("r={o!r}\ns={o}\nid={o.identifier}\nj={j}".format(o=o, j=to_json(o)))
+
+    genotypes = {
+        "{}/{}".format(h1n, h2n): Genotype([h1, h2])
+        for h1n, h1 in haplotypes.items()
+        for h2n, h2 in haplotypes.items()
+        }
+    o = genotypes["ε1/ε1"]
+    print("r={o!r}\ns={o}\nid={o.identifier}\nj={j}".format(o=o, j=to_json(o)))
